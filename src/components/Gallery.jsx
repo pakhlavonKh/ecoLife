27278476024ea89177dc4014bc6ecfdcd433b78e
Pakhlavon Khamidov <76khwmidov@gmail.com>
2025-08-01 19:43:35 +0500

    photos changed

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Gallery() {
   const images = [
-    "https://ik.imagekit.io/hyp089vmms/assets/gallery-1.webp?tr=f-auto,q-80",
+    "https://ik.imagekit.io/hyp089vmms/assets/gallery-1.webp?updatedAt=1754059249466?tr=f-auto,q-80",
     "https://ik.imagekit.io/hyp089vmms/assets/gallery-3.webp?tr=f-auto,q-80",
     "https://ik.imagekit.io/hyp089vmms/assets/gallery-4.webp?tr=f-auto,q-80",
     "https://ik.imagekit.io/hyp089vmms/assets/gallery-5.webp?tr=f-auto,q-80",
     "https://ik.imagekit.io/hyp089vmms/assets/gallery-6.webp?tr=f-auto,q-80",
     "https://ik.imagekit.io/hyp089vmms/assets/gallery-7.webp?tr=f-auto,q-80",
-    "https://ik.imagekit.io/hyp089vmms/assets/gallery-8.webp?tr=f-auto,q-80",
+    "https://ik.imagekit.io/hyp089vmms/assets/gallery-8.webp?updatedAt=1754059338146?tr=f-auto,q-80",
     "https://ik.imagekit.io/hyp089vmms/assets/gallery-9.webp?tr=f-auto,q-80",
     "https://ik.imagekit.io/hyp089vmms/assets/gallery-10.webp?tr=f-auto,q-80",
     "https://ik.imagekit.io/hyp089vmms/assets/gallery-11.webp?tr=f-auto,q-80",
